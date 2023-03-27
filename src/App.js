c8de3d0cf31c2367b23011fe68cb2603e9406e18
alikameli <alikameli0123@gmail.com>
2023-03-27 18:07:48 +0430

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import Data from './api/Data';
 import './App.css';
 import Game from './components/Game/Game';
 import GameRoute from './components/GameRoute/GameRoute';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  console.log(Data.robotAccessKey());
   return (
     <div className="App">
-      {/* <Route path="/" element={<App />}> */}
-      {/* <Route> */}
       <Routes>
-
         <Route path="/game/:id" element={<Game />} />
         <Route path="/" element={<GameRoute />} />
       </Routes>
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
 import style from "./Game.module.css";
 
 const Game = () => {
+    const params = useParams();
+    const id = params.id;
+    const firstTxt = useRef();
+    const secTxt = useRef();
+    const threeTxt = useRef();
+    const fourTxt = useRef();
+    const fiveTxt = useRef();
+
+    const handleFocus = (e) => {
+        if (e.target.nextSibling) e.target.nextSibling.focus();
+    }
+
+    const formHandler = () => {
+        const firstElement = firstTxt.current.value;
+        const secElement = secTxt.current.value;
+        const threeElement = threeTxt.current.value;
+        const fourElement = fourTxt.current.value;
+        const fiveElement = fiveTxt.current.value;
+
+        const txt = firstElement + secElement + threeElement + fourElement + fiveElement;
+        console.log(txt);
+    }
+
     return (
         <div>
-            hi
+            <div className={style.sky}>
+                <div className={style.step}>{id}</div>
+            </div>
+
+            <div className={style.inputs}>
+                <from className={style.form} onChange={formHandler} >
+                    <input autoFocus={true} ref={firstTxt} onChange={handleFocus} type="text" maxLength="1" />
+                    <input onChange={handleFocus} ref={secTxt} type="text" maxLength="1" />
+                    <input onChange={handleFocus} ref={threeTxt} type="text" maxLength="1" />
+                    <input onChange={handleFocus} ref={fourTxt} type="text" maxLength="1" />
+                    <input onChange={handleFocus}  ref={fiveTxt} type="text" maxLength="1" />
+                </from>
+                {/* <div className={style.}></div> */}
+            </div>
         </div>
     )
 }
